778100aeee7c7dfdd87bab96b26802a6a5ed8af7
Elisabet Fredling <e.fredling@gmail.com>
2020-02-09 22:10:43 +0000

    kmom03 cmt-4, 4 mdf files

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -30,6 +30,11 @@ return [
                         "url" => "redovisning/kmom02",
                         "title" => "Redovisning för kmom02.",
                     ],
+                    [
+                        "text" => "kmom03",
+                        "url" => "redovisning/kmom03",
+                        "title" => "Redovisning för kmom03.",
+                    ],
                 ],
             ],
         ],
